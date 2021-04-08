2dec5b40ba6ee73483459bad019c3229477bd10c
Yin <yinnnn17@gmail.com>
2021-04-07 18:33:29 -0700

    Implemented S3 file uploads

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -2,25 +2,28 @@ if (document.querySelector('#new-pet')) {
   document.querySelector('#new-pet').addEventListener('submit', (e) => {
       e.preventDefault();
 
-      let pet = {};
-      const inputs = document.querySelectorAll('.form-control');
-      for (const input of inputs) {
-          pet[input.name] = input.value;
-      }
+      // Use FormData to grab everything now that we have files mixed in with text
+      let form = document.getElementById("new-pet");
+      let pet = new FormData(form);
 
-      axios.post('/pets', pet)
+      // Assign the multipart/form-data headers to axios does a proper post
+      axios.post('/pets', pet, {
+          headers: {
+              'Content-Type': 'multipart/form-data;'
+          }
+      })
           .then(function (response) {
-              window.location.replace(`/pets/${response.data._id}`);
+              window.location.replace(`/pets/${response.data.pet._id}`);
           })
           .catch(function (error) {
-            const alert = document.getElementById('alert')
-            alert.classList.add('alert-warning');
-            alert.textContent = 'Oops, something went wrong saving your pet. Please check your information and try again.';
-            alert.style.display = 'block';
-            setTimeout(() => {
-              alert.style.display = 'none';
-              alert.classList.remove('alert-warning');
-             }, 3000)
+              const alert = document.getElementById('alert')
+              alert.classList.add('alert-warning');
+              alert.textContent = 'Oops, something went wrong saving your pet. Please check your information and try again.';
+              alert.style.display = 'block';
+              setTimeout(() => {
+                  alert.style.display = 'none';
+                  alert.classList.remove('alert-warning');
+              }, 3000)
           });
   });
 }
--- routes/pets.js
+++ routes/pets.js
@@ -1,6 +1,11 @@
 // MODELS
 const Pet = require('../models/pet');
 
+// UPLOADING TO AWS S3
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+const Upload = require('s3-uploader');
+
 // PET ROUTES
 module.exports = (app) => {
 
@@ -25,18 +30,30 @@ module.exports = (app) => {
   });
 
   // CREATE PET
-  app.post('/pets', (req, res) => {
+  app.post('/pets', upload.single('avatar'), (req, res, next) => {
     var pet = new Pet(req.body);
+    pet.save(function (err) {
+      if (req.file) {
+        // Upload the images
+        client.upload(req.file.path, {}, function (err, versions, meta) {
+          if (err) { return res.status(400).send({ err: err }) };
 
-    pet.save()
-      .then((pet) => {
+          // Pop off the -square and -standard and just use the one URL to grab the image
+          versions.forEach(function (image) {
+            var urlArray = image.url.split('-');
+            urlArray.pop();
+            var url = urlArray.join('-');
+            pet.avatarUrl = url;
+            pet.save();
+          });
+
+          res.send({ pet: pet });
+        });
+      } else {
         res.send({ pet: pet });
-      })
-      .catch((err) => {
-        // STATUS OF 400 FOR VALIDATIONS
-        res.status(400).send(err.errors);
-      }) ;
-  });
+      }
+    })
+  })
 
   // SHOW PET
   app.get('/pets/:id', (req, res) => {
@@ -71,3 +88,25 @@ module.exports = (app) => {
   });
 }
 
+const client = new Upload(process.env.S3_BUCKET, {
+  aws: {
+    path: 'pets/avatar',
+    region: process.env.S3_REGION,
+    acl: 'public-read',
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  },
+  cleanup: {
+    versions: true,
+    original: true
+  },
+  versions: [{
+    maxWidth: 400,
+    aspect: '16:10',
+    suffix: '-standard'
+  },{
+    maxWidth: 300,
+    aspect: '1:1',
+    suffix: '-square'
+  }]
+});
